19782f0f3b57d4e12d9063423a1c4f521c3ecb6a
WojciechO <wojciech.orchowski@gmail.com>
2018-03-07 00:10:22 +0100

    Script runs for example. Incorrect movement. Car/Ride statuses for refactor

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-#Runs the algorithm on specified input file
+#
+# Runs the algorithm on specified input file
+
+import utils
+import ride as ridez
+import car as carz
+
+R, C, B, T, cars, rides = utils.read_input('a_example.in')
+
+time = 0
+while time < T:
+
+    for car in cars:
+        if car.status == carz.CarStatus.BENCH:
+            for ride in rides:
+                if ride.status == ridez.RideStatus.WAITING_FOR_ASSIGNMENT:
+                    car.assign_ride(ride)
+
+        car.move(time)
+
+    time += 1
+
 
--- utils.py
+++ utils.py
@@ -1,23 +1,28 @@
 from ride import Ride
-
+from car import Car
 
 def read_input(fname):
     with open('./In/{}'.format(fname), 'r')as reader:
         R, C, F, N, B, T = [int(i) for i in reader.readline().split(" ")]
 
+        cars = []
+
+        for car in range(0, F):
+            cars.append(Car())
+
         rides = []
         for ride in range(0, N):
-            a, b, x, y, s, f = reader.readline().split(" ")
+            a, b, x, y, s, f = [int(i) for i in reader.readline().split(" ")]
             rides.append(Ride(a, b, x, y, s, f))
 
-    return R, C, F, N, B, T, rides
+    return R, C, B, T, cars, rides
 
 def write_output(results):
     pass
 
 
 def taxi_metric(point1, point2):
-    pass
+    return abs(point1[0]-point2[0]) + abs(point1[1]-point2[1])
 
 
 if __name__ == "__main__":
